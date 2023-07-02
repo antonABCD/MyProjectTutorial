04601fb947c5c5b0752733ea8d0c7245f875c180
antonABCD <anton.bilaniuk.com.ua>
2023-07-02 19:14:43 +0300

    New Function

--- Namefunc/Namefunc/Namefunc.cpp
+++ Namefunc/Namefunc/Namefunc.cpp
@@ -1,14 +1,23 @@
 // Namefunc.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
 #include <iostream>
+
+
 void doSomething() {
     std::cout << "Hi!";
 }
+
+void print() {
+    int a = 8;
+    std::cout << a;
+}
+
 int main()
 {
     std::cout << "Hello World!\n";
     doSomething();
+    print();
+    return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
